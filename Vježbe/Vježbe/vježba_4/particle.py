65886ea6218c22f9cb9715054e7f07dee6ac4e9f
DujoFG <dfilipovi@pmfst.hr>
2025-04-10 11:30:23 +0200

    Correction of def range()& C:/Users/HP/AppData/Local/Programs/Python/Python313/python.exe c:/Users/HP/Documents/PAF/PAF/Vježbe/Vježbe/vježba_4/particle.py

--- Vježbe/Vježbe/vježba_4/particle.py
+++ Vježbe/Vježbe/vježba_4/particle.py
@@ -1,10 +1,11 @@
 import math as m
 
 class Particle:
-    def __init__(self,v,fi,r):
+    def __init__(self,v,fi,r): #r=[x,y]
         self.fi=m.radians(fi)
         self.r=r
         self.v=[v*m.cos(self.fi),v*m.sin(self.fi)]
+        print(self.fi,self.r,self.v)
     
     def reset(self):
        self.v=None
@@ -14,25 +15,45 @@ class Particle:
     def __move(self,dt):
         self.r[0]=self.r[0]+self.v[0]*dt
         return self.r[0]
-
-    def range(self,dt):
+    
+    def range(self):
+        dt=0.0001
         g=-9.81
         x=[self.r[0]]
         y=[self.r[1]]
         vy=[self.v[1]]
         i=0
-        while self.r[1]!=0 or self.v[1]>=0:
+        while self.r[1]>=0 or self.v[1]>=0:
             self.r[0]=x[i]+self.v[0]*dt
             x.append(self.r[0])
-            self.r[1]=y[i]+vy[i]
+            self.r[1]=y[i]+vy[i]*dt
             y.append(self.r[1])
             self.v[1]=vy[i]+g*dt
             vy.append(self.v[1])
-            D=self.r[0]
+            D=x[i]
             i+=1
-        print(D)
+        print("Domet:",D)
 
+    def plot_trajectory(self,dt):
+        g=-9.81
+        x=[self.r[0]]
+        y=[self.r[1]]
+        vy=[self.v[1]]
+        i=0
+        while self.r[1]>=0 or self.v[1]>=0:
+            self.r[0]=x[i]+self.v[0]*dt
+            x.append(self.r[0])
+            self.r[1]=y[i]+vy[i]*dt
+            y.append(self.r[1])
+            self.v[1]=vy[i]+g*dt
+            vy.append(self.v[1])
+            i+=1
 
+p=Particle(10,30,[2,2])
+p.range()
+print("Kut:",p.fi)
+print("Mjesto:",p.r)
+print("Brzina:",p.v)
 
 
 
