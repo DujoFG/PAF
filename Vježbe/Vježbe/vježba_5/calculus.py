5f5b734b1129b0c9028e702e860eee6328141df2
DujoFG <dfilipovi@pmfst.hr>
2025-05-06 23:24:08 +0200

    Zad1-2.metoda

--- Vježbe/Vježbe/vježba_5/calculus.py
+++ Vježbe/Vježbe/vježba_5/calculus.py
@@ -1,8 +1,10 @@
+import numpy as np
+
 class Calculus:
     def __init__(self,f):
         self.f=f
 
-    def derivacija(self,P,m=None):
+    def d_tocka(self,P,m=None):
         h=0.001
         x=P[0]
         y=P[1]
@@ -10,14 +12,24 @@ class Calculus:
             df=(self.f(x+h)-self.f(x-h))/(2*h)
         else:
             df=(self.f(x+h)-y)/h
-            print("2")
         print(df)
         return df
     
+    def d_int(self,dg,gg,h,m=None):
+        točke=[]
+        df_t=[]
+        for x in np.linspace(dg,gg,20):
+            if m is None:
+                df=(self.f(x+h)-self.f(x-h))/(2*h)
+                točke.append(x)
+                df_t.append(df)
+            else:
+                df=(self.f(x+h)-self.f(x))/h
+                točke.append(x)
+                df_t.append(df)
+        print(točke,df_t)
+        return točke,df_t
 
-def f(x):
-    return 2*x-3
-c=Calculus(f)
-c.derivacija([0,-3],1)
+
 
 
