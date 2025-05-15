7885ce597ff2d90c9464720ffac9389311484339
DujoFG <dfilipovi@pmfst.hr>
2025-05-16 00:17:08 +0200

    Završen zad 1- vježba 6

--- Vježbe/Vježbe/vježba_6/harmonic_oscillator.py
+++ Vježbe/Vježbe/vježba_6/harmonic_oscillator.py
@@ -1,58 +1,32 @@
 import matplotlib.pyplot as plt
-import numpy as np
 
 class HarmonicOscillator:
     def __init__(self, x0, v0 , m, k, dt, T):
-        self.m = m
-        self.k = k
-        self.x0 = x0
-        self.v0 = v0
-        self.dt = dt
-        self.T = T
-        self.omega = np.sqrt(k / m)
-        self.N = int(T / dt)
-        self.t = np.linspace(0, T, self.N)
-        self.te=[]
-        self.a=[]
-        self.v=[]
-        self.x=[]
-
-    def oscilator(self):
-        t=1
-        i=0
+        self.m=m
+        self.k=k
+        self.x0=x0
+        self.v0=v0
+        self.dt=dt
+        self.T=T
         self.x=[self.x0]
         self.v=[self.v0]
-        self.a=[-(self.k/self.m)*self.x[0]]
-        self.te=[0]
+        self.a=[-(k/m)*x0]
+        self.t=[0]
+
+    def oscilator(self):
+        t=self.dt
+        i=0
         while t<=self.T:
             self.v.append(self.v[i]+self.a[i]*self.dt)
-            self.x.append(self.x[i]+self.v[i]*self.dt)
-            self.a.append(-(self.k/self.m)*self.x[i])
-            self.te.append(t)
+            self.x.append(self.x[i]+self.v[i+1]*self.dt)
+            self.a.append(-(self.k/self.m)*self.x[i+1])
+            self.t.append(t)
             t+=self.dt
             i+=1
-        return [self.te,self.x,self.v,self.a]
-    
-    def analytical_solution(self):
-        self.omega = np.sqrt(self.k / self.m)
-        x = self.x0 * np.cos(self.omega * self.t) + (self.v0 / self.omega) * np.sin(self.omega * self.t)
-        v = -self.x0 * self.omega * np.sin(self.omega * self.t) + self.v0 * np.cos(self.omega * self.t)
-        a = -self.omega**2 * x
-        return [self.t,x, v, a]
+        return [self.t,self.x,self.v,self.a]
 
 
-c=HarmonicOscillator(0.3,0,1,1,0.01,20)
-h=c.oscilator()
-an=c.analytical_solution()
-plt.plot(h[0],h[1],color="red")
-plt.scatter(an[0],an[1])
-plt.show()
-plt.plot(h[0],h[2],color="green")
-plt.scatter(an[0],an[2])
-plt.show()
-plt.plot(h[0],h[3],color="black")
-plt.scatter(an[0],an[3])
-plt.show()
+
             
 
 
---
+++ Vježbe/Vježbe/vježba_6/oscilator.py
@@ -0,0 +1,29 @@
+import harmonic_oscillator as ho
+import matplotlib.pyplot as plt
+import math as m
+
+for dt in [0.01,0.05,0.1]:
+    c=ho.HarmonicOscillator(0.3,0,1,10,dt,20)
+    h=c.oscilator()
+
+    def analitičko(t,x0,m0,k):
+        w=m.sqrt(k/m0)
+        return [x0*m.cos(w*t),-w*x0*m.sin(w*t),-w**2*x0*m.cos(w*t)]
+    x=[]
+    v=[]
+    a=[]
+    for t in h[0]:
+        an=analitičko(t,0.3,1,10)
+        x.append(an[0])
+        v.append(an[1])
+        a.append(an[2])
+
+    plt.plot(h[0],h[1],color="red")
+    plt.plot(h[0],x,ls='--')
+    plt.show()
+    plt.plot(h[0],h[2],color="green")
+    plt.plot(h[0],v,ls='--')
+    plt.show()
+    plt.plot(h[0],h[3],color="black")
+    plt.plot(h[0],a,ls='--')
+    plt.show()
