312bd579876f3153228012f210e966afaec6bed7
DujoFG <dfilipovi@pmfst.hr>
2025-05-21 21:51:22 +0200

    Završena vježba 7

--- Vježbe/Vježbe/vježba_7/preciznost.py
+++ Vježbe/Vježbe/vježba_7/preciznost.py
@@ -2,21 +2,25 @@ import projectile as pro
 import matplotlib.pyplot as plt
 
 p=pro.Projectile(45,10,[0,0],1.225,1.05,1,1)
-x,vx,ax,y,vy,ay,t=p.kosi_hitac(0.1)
+x,vx,aex,y,vy,aey,t=p.kosi_hitac(0.1)
 plt.plot(x,y,color="black",lw=1,label="Euler-0.1s")
 plt.xlabel("x/[m]")
 plt.ylabel("y/[m]")
 
-x,vx,ax,y,vy,ay,t=p.kosi_hitac(0.01)
+x,vx,aex,y,vy,aey,t=p.kosi_hitac(0.01)
 plt.plot(x,y,color="blue",lw=1,label="Euler-0.01s")
 plt.xlabel("x/[m]")
 plt.ylabel("y/[m]")
 
-x,vx,ax,y,vy,ay,t=p.kosi_hitac(0.001)
+x,vx,aex,y,vy,aey,t=p.kosi_hitac(0.001)
 plt.plot(x,y,color="green",lw=1,label="Euler-0.001s")
 plt.xlabel("x/[m]")
 plt.ylabel("y/[m]")
 
-plt.title("Eulerova metoda")
+x,vx,y,vy,t=p.Runge_Kutta(0.01)
+plt.plot(x,y,color="red",lw=1,label="Runge-Kutta-0.01s")
+plt.xlabel("x/[m]")
+plt.ylabel("y/[m]")
+
 plt.legend()
 plt.show()
--- Vježbe/Vježbe/vježba_7/projectile.py
+++ Vježbe/Vježbe/vježba_7/projectile.py
@@ -11,8 +11,8 @@ class Projectile:
         self.vy=[v0*m.sin(self.fi0)]
         self.x=[r0[0]]
         self.y=[r0[1]]
-        self.ax=[0]
-        self.ay=[-9.81]
+        self.aex=[0]
+        self.aey=[-9.81]
         self.t=[0]
         self.ro=ro
         self.Cd=Cd
@@ -24,8 +24,8 @@ class Projectile:
         self.vy=[self.v0*m.sin(self.fi0)]
         self.x=[self.r0[0]]
         self.y=[self.r0[1]]
-        self.ax=[0]
-        self.ay=[-9.81]
+        self.aex=[0]
+        self.aey=[-9.81]
         self.t=[0]
     
     def kosi_hitac(self,dt):
@@ -33,16 +33,16 @@ class Projectile:
         i=0
         t=dt
         while self.y[i]>=0:
-            self.ax.append(-np.sign(self.vx[i])*((self.ro*self.Cd*self.A)/(2*self.m0))*(self.vx[i])**2)
-            self.vx.append(self.vx[i]+self.ax[i]*dt)
+            self.aex.append(-np.sign(self.vx[i])*((self.ro*self.Cd*self.A)/(2*self.m0))*(self.vx[i])**2)
+            self.vx.append(self.vx[i]+self.aex[i]*dt)
             self.x.append(self.x[i]+self.vx[i+1]*dt)
-            self.ay.append(-9.81-np.sign(self.vy[i])*((self.ro*self.Cd*self.A)/(2*self.m0))*(self.vy[i])**2)
-            self.vy.append(self.vy[i]+self.ay[i]*dt)
+            self.aey.append(-9.81-np.sign(self.vy[i])*((self.ro*self.Cd*self.A)/(2*self.m0))*(self.vy[i])**2)
+            self.vy.append(self.vy[i]+self.aey[i]*dt)
             self.y.append(self.y[i]+self.vy[i+1]*dt)  
             self.t.append(t)
             t+=dt
             i+=1
-        return self.x,self.vx,self.ax,self.y,self.vy,self.ay,self.t
+        return self.x,self.vx,self.aex,self.y,self.vy,self.aey,self.t
     
     def ax(self,vx):
         return -np.sign(vx)*((self.ro*self.Cd*self.A)/(2*self.m0))*(vx)**2
@@ -81,9 +81,4 @@ class Projectile:
         return self.x,self.vx,self.y,self.vy,self.t
 
 
-p=Projectile(45,10,[0,0],1.225,1.05,1,1)
-x,vx,y,vy,t=p.Runge_Kutta(0.01)
-plt.plot(x,y,color="red")
-x,vx,ax,y,vy,ay,t=p.kosi_hitac(0.001)
-plt.plot(x,y)
-plt.show()
+
