342088f26cf5e8aafa07e0476a512e168d466408
DujoFG <dfilipovi@pmfst.hr>
2025-05-23 11:36:17 +0200

    Vježba 8 skoro završena

--- Vježbe/Vježbe/vježba_8/gibanje_u_EM_polju.py
+++ Vježbe/Vježbe/vježba_8/gibanje_u_EM_polju.py
@@ -21,29 +21,63 @@ class Gibanje_u_EM_polju:
         self.r=[np.array(self.r0)]
         self.t=[0]
 
-    def gibanje(self,dt,T):
+    def gibanje_Euler(self,dt,T):
+        self.reset()
         N=int(T/dt)
         t=dt
         for i in range(0,N+1):
-            self.a.append((self.q/self.m0)*(self.E+np.cross(self.v[i],self.B)))
+            self.a.append((self.q/self.m0)*(np.cross(self.v[i],self.B)))
             self.v.append(self.v[i]+self.a[i]*dt)
             self.r.append(self.r[i]+self.v[i+1]*dt)
             self.t.append(t)
             t+=dt
         return self.a,self.v,self.r,self.t
 
+    def aRK(self,v):
+        return (self.q/self.m0)*(np.cross(v,self.B))
+
+    def gibanje_RK(self,dt,T):
+        self.reset()
+        N=int(T/dt)
+        t=dt
+        for i in range(0,N+1):
+            k1v=self.aRK(self.v[i])*dt
+            k1r=self.v[i]*dt
+            k2v=self.aRK(self.v[i]+(k1v/2))*dt
+            k2r=(self.v[i]+(k1v/2))*dt
+            k3v=self.aRK(self.v[i]+(k2v/2))*dt
+            k3r=(self.v[i]+(k2v/2))*dt
+            k4v=self.aRK(self.v[i]+k3v)*dt
+            k4r=(self.v[i]+k3v)*dt
+            self.v.append(self.v[i]+(1/6)*(k1v+2*k2v+2*k3v+k4v))
+            self.r.append(self.r[i]+(1/6)*(k1r+2*k2r+2*k3r+k4r))
+            self.t.append(t)
+            t+=dt
+        return self.r,self.v,self.t
 
 g=Gibanje_u_EM_polju((0,0,0),(0.1,0.1,0.1),(0,0,0),(0,0,1),1,-1)
-a,v,r,t=g.gibanje(0.01,20)
-x=[p[0] for p in r]
-y=[p[1] for p in r]
-z=[p[2] for p in r]
+a,v,r,t=g.gibanje_Euler(0.01,20)
+xe=[p[0] for p in r]
+ye=[p[1] for p in r]
+ze=[p[2] for p in r]
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-ax.plot(x, y, z)
+ax.plot(xe, ye, ze)
 ax.set_xlabel('x')
 ax.set_ylabel('y')
 ax.set_zlabel('z')
+
+r,v,t=g.gibanje_RK(0.01,20)
+xrk=[p[0] for p in r]
+yrk=[p[1] for p in r]
+zrk=[p[2] for p in r]
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot(xrk, yrk, zrk)
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('z')
+
 plt.show()
 
         
