00e95c13a0c365c4c02f3c3ecb44825808634902
DujoFG <dfilipovi@pmfst.hr>
2025-05-31 15:44:57 +0200

    Završena-Vj.9

--- Vježbe/Vježbe/vježba_9/gravitacija.py
+++ Vježbe/Vježbe/vježba_9/gravitacija.py
@@ -2,36 +2,61 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class Gravitacija:
-    def __init__(self, Mz, Ms, v0z, v0s, r0z, r0s): #r0z, r0s=[x0,y0]; v0z,v0s=[v0x,v0y]
-        self.Ms=Ms
+    def __init__(self, Mz, Ms, v0z, v0s, r0z, r0s): #r0z,r0s=[x0,y0]; v0z,v0s=[v0x,v0y])
+        self.v0z=v0z
+        self.v0s=v0s
+        self.r0z=r0z
+        self.r0s=r0s
+        self.Ms=Ms  
         self.Mz=Mz
-        self.aS=[]
-        self.aZ=[]
-        self.vS=[v0s]
-        self.vZ=[v0z]
-        self.rS=[r0s]
-        self.rZ=[r0z]
-        self.t=[]
+        self.aS=[np.array([0,0])]
+        self.aZ=[np.array([0,0])]
+        self.vS=[np.array(v0s)]
+        self.vZ=[np.array(v0z)]
+        self.rS=[np.array(r0s)]
+        self.rZ=[np.array(r0z)]
+        self.t=[0]
+    
+    def reset(self):
+        self.aS=[np.array([0,0])]
+        self.aZ=[np.array([0,0])]
+        self.vS=[np.array(self.v0s)]
+        self.vZ=[np.array(self.v0z)]
+        self.rS=[np.array(self.r0s)]
+        self.rZ=[np.array(self.r0z)]
+        self.t=[0]
+        
+    def akc(self,rZ,rS):
+        G=6.67408*10**(-11)
+        aZ=(-G*self.Ms*(rZ-rS)/np.sqrt((rZ[0]-rS[0])**2+(rZ[1]-rS[1])**2)**3)
+        aS=(-G*self.Mz*(rS-rZ)/np.sqrt((rS[0]-rZ[0])**2+(rS[1]-rZ[1])**2)**3)
+        return aZ,aS
 
     def korak(self,dt,T):
-        G=6.67408*10**(-11)
-        t=0
+        self.reset()
+        Ts=T*24*3600
+        t=dt
         i=0
-        while t<=T:
-            self.aZ.append((-G*self.Ms*(self.rZ[i]-self.rS[i])/np.sqrt((self.rZ[i])**2-(self.rS[i])**2)**3))
-            self.vZ.append(self.vZ[i]+self.aZ[i])
-            self.rZ.append(self.rZ[i]+self.vZ[i+1])
-            self.aS.append((-G*self.Mz*(self.rS[i]-self.rZ[i])/np.sqrt((self.rS[i])**2-(self.rZ[i])**2)**3))
-            self.vS.append(self.vS[i]+self.aS[i])
-            self.rS.append(self.rS[i]+self.vS[i+1])
+        while t<=Ts:
+            aZ,aS=self.akc(self.rZ[i],self.rS[i])
+            self.aZ.append(aZ)
+            self.vZ.append(self.vZ[i]+self.aZ[i]*dt)
+            self.rZ.append(self.rZ[i]+self.vZ[i+1]*dt)
+            self.aS.append(aS)
+            self.vS.append(self.vS[i]+self.aS[i]*dt)
+            self.rS.append(self.rS[i]+self.vS[i+1]*dt)
             self.t.append(t)
             i+=1
             t+=dt
         return self.rS,self.rZ
     
 G=Gravitacija(5.9742*10**24, 1.989*10**30, [0,29783], [0,0], [1.486*10**11,0], [0,0])
-S,Z=G.korak(0.01,365.24)
+S,Z=G.korak(450,365.24)
 xz=[r[0] for r in Z]
 yz=[r[1] for r in Z]
-plt.plot(xz,yz)
+xs=[r[0] for r in S]
+ys=[r[1] for r in S]
+plt.plot(xz,yz,label="Zemlja")
+plt.plot(xs,ys,color="yellow",lw=2,label="Sunce")
+plt.legend()
 plt.show()
