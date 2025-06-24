895b4ce08678a4c9bc503111e2fe916760cea702
DujoFG <dfilipovi@pmfst.hr>
2025-06-24 16:12:54 +0200

    Seminar završen

--- Vježbe/Vježbe/seminar/universe.py
+++ Vježbe/Vježbe/seminar/universe.py
@@ -1,121 +1,119 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 class Planet:
-    def __init__(self,Mp,r0,v0): #r0=[x0,y0], v0=[vx0,vy0]
+    def __init__(self,Mp,r0,v0,T0,ime): #r0=[x0,y0], v0=[vx0,vy0], ime="Zemlja", T0-dani
         self.Mp=Mp
         self.r0=np.array(r0)
         self.v0=np.array(v0)
+        self.r=[self.r0]
+        self.v=[self.v0]
+        self.a=[]
+        self.T0=T0
+        self.ime=ime
 
-    def podaci(self):
-        return self.r0, self.v0, self.Mp
-
-    def sila_na_Planet(self,rp1,r_dp,m_dp):
-        Fuk=0
-        G=6.67*10**(-11)
-        for n, rp2 in enumerate(r_dp):
-            if not np.array_equal(rp1, rp2):
-                udaljenost=np.sqrt((rp1[0]-rp2[0])**2+(rp1[1]-rp2[1])**2)
-                smjersile=(rp2-rp1)/udaljenost
-                Fuk+=((G*self.Mp*m_dp[n])/udaljenost**2)*smjersile
-        return Fuk
+    def pod_za_gib(self):
+        return self.r0, self.v0, self.Mp, self.ime
     
-    def pomak(self,rp1,vp1,r_dp,m_dp,dt):
-        a=self.sila_na_Planet(rp1,r_dp,m_dp)/self.Mp
-        v=vp1+a*dt
-        r=rp1+v*dt
-        return r,v,a
-  
+    def Podaci_o_Planetu(self):
+        ime="Ime planeta: {}".format(self.ime)
+        masa="Masa: {}".format(self.Mp)
+        god="Period kruženja: {}".format(self.T0)
+        d="Udaljenost od Sunca: {}".format(self.r0)
+        brzina="Brzina kruženja: {}".format(self.v0)
+        return ime,masa,god,d,brzina
+
 class Universe:
-    def __init__(self,Sunce,Merkur,Venera,Zemlja,Mars):#Sunce=Mp,[x,y],[vx,vy]
-        self.Sunce=Planet(*Sunce)
-        self.Merkur=Planet(*Merkur)
-        self.Venera=Planet(*Venera)
-        self.Zemlja=Planet(*Zemlja)
-        self.Mars=Planet(*Mars)
+    def __init__(self):
+        self.tijela=[]
 
-    def početni_podaci(self):
-        rs,vs,ms=self.Sunce.podaci()
-        rm,vm,mm=self.Merkur.podaci()
-        rv,vv,mv=self.Venera.podaci()
-        rz,vz,mz=self.Zemlja.podaci()
-        rM,vM,mM=self.Mars.podaci()
-        r0_p=[rs,rm,rv,rz,rM]
-        v0_p=[vs,vm,vv,vz,vM]
-        m_p=[ms,mm,mv,mz,mM]
-        return r0_p,v0_p,m_p
+    def dodaj_tijela(self, *tijela):
+        for tijelo in tijela:
+            self.tijela.append(tijelo)
+
+    def akceleracija(self):
+        G=6.67*10**(-11)
+        a_t=[]
+        for tijelo in self.tijela:
+            auk=0
+            for t in self.tijela:
+                if t!=tijelo:
+                    udaljenost=np.sqrt((tijelo.r[-1][0]-t.r[-1][0])**2+(tijelo.r[-1][1]-t.r[-1][1])**2)
+                    smjerakc=(t.r[-1]-tijelo.r[-1])/udaljenost
+                    auk+=((G*t.Mp)/udaljenost**2)*smjerakc
+            a_t.append(auk)
+        return a_t
     
-    def gibanje(self,dt,T):
-        r0_p,v0_p,m_p=self.početni_podaci()
-        rg=[]
-        vg=[]
-        ag=[]
-        tt=[]
-        r_p=[r0_p]
-        v_p=[v0_p]
-        i=0
+    def pomak(self,dt):
+        a_t=self.akceleracija()
+        for i,tijelo in enumerate(self.tijela):
+            tijelo.a.append(a_t[i])
+            tijelo.v.append(tijelo.v[-1]+tijelo.a[-1]*dt)
+            tijelo.r.append(tijelo.r[-1]+tijelo.v[-1]*dt)
+
+    def gibanje(self,dt,T):#T u godinama,dt u sekundama
+        Tp=T*365*24*3600
         t=0
-        while t<=T:
-            rS,vS,aS=self.Sunce.pomak(r_p[i][0],v_p[i][0],r_p[i],m_p,dt)
-            rm,vm,am=self.Merkur.pomak(r_p[i][1],v_p[i][1],r_p[i],m_p,dt)
-            rV,vV,aV=self.Venera.pomak(r_p[i][2],v_p[i][2],r_p[i],m_p,dt)
-            rZ,vZ,aZ=self.Zemlja.pomak(r_p[i][3],v_p[i][3],r_p[i],m_p,dt)
-            rM,vM,aM=self.Mars.pomak(r_p[i][4],v_p[i][4],r_p[i],m_p,dt)
-            rg.append([rS,rm,rV,rZ,rM])
-            vg.append([vS,vm,vV,vZ,vM])
-            ag.append([aS,am,aV,aZ,aM])
-            v_p.append([vS,vm,vV,vZ,vM])
-            r_p.append([rS,rm,rV,rZ,rM])
-            tt.append(t)
-            i+=1
+        while t<=Tp:
+            for tijelo in self.tijela:
+                self.pomak(dt)
             t+=dt
-        return rg,vg,ag,tt
 
-    def putanja(self,dt,T):#T u godinama,dt u sekundama
-        Tp=T*365*24*3600
-        rg,vg,ag,tt=self.gibanje(dt,Tp)
-        rs=[]
-        rm=[]
-        rv=[]
-        rz=[]
-        rM=[]
-        for r in rg:
-            rs.append(r[0])
-            rm.append(r[1])
-            rv.append(r[2])
-            rz.append(r[3])
-            rM.append(r[4])
-
-            plt.plot([r[0] for r in rs],[r[1] for r in rs],color="yellow",label="Sunce")
-            plt.plot([r[0] for r in rm],[r[1] for r in rm],color="purple",label="Merkur")
-            plt.plot([r[0] for r in rv],[r[1] for r in rv],color="green",label="Venera")
-            plt.plot([r[0] for r in rz],[r[1] for r in rz],color="blue",label="Zemlja")
-            plt.plot([r[0] for r in rM],[r[1] for r in rM],color="red",label="Mars")
-            plt.pause(0.0001)
-        plt.show()
-    
     def položaj(self,dt,T):
-        Tp=T*365*24*3600
-        rg,vg,ag,tt=self.gibanje(dt,Tp)
-        rs=[]
-        rm=[]
-        rv=[]
-        rz=[]
-        rM=[]
-        for r in rg:
-            rs.append(r[0])
-            rm.append(r[1])
-            rv.append(r[2])
-            rz.append(r[3])
-            rM.append(r[4])
-
-        plt.scatter(rs[-1][0],rs[-1][1],color="yellow",label="Sunce",s=5)
-        plt.scatter(rm[-1][0],rm[-1][1],color="purple",label="Merkur",s=5)
-        plt.scatter(rv[-1][0],rv[-1][1],color="green",label="Venera",s=5)
-        plt.scatter(rz[-1][0],rz[-1][1],color="blue",label="Zemlja",s=5)
-        plt.scatter(rM[-1][0],rM[-1][1],color="red",label="Mars",s=5)
+        self.gibanje(dt,T)
+        boja=['yellow','grey','orange','blue','red']
+        for i,tijelo in enumerate(self.tijela):
+            r,v,m,ime=tijelo.pod_za_gib()
+            if ime=="Sunce":
+                plt.scatter(tijelo.r[-1][0],tijelo.r[-1][1],color=boja[i],label=ime,s=100)
+            else:
+                plt.scatter(tijelo.r[-1][0],tijelo.r[-1][1],color=boja[i],label=ime,s=20)
+        plt.xlabel("x [m]")
+        plt.ylabel("y [m]")
+        plt.title("Položaj planeta nakon {} godine".format(T))
+        plt.legend()
+        plt.axis("equal")
+        plt.grid(True)
         plt.show()
 
-        
-U=Universe([1.989e30,[0,0],[0,0]],[3.285e23,[57.91e9,0],[0,47.9e3]],[4.867e24,[108.2e9,0],[0,35.2e3]],[6e24,[149.6e9,0],[0,29.8e3]],[6.4191e23,[228e9,0],[0,24.077e3]])
-U.putanja(115200,1)
+    def simulacija(self,dt,T):
+        self.gibanje(dt, T)
+        fig, ax = plt.subplots()
+        ax.set_aspect('equal')
+        ax.set_xlim(-2.5e11, 2.5e11)
+        ax.set_ylim(-2.5e11, 2.5e11)
+
+        boje = ['yellow', 'gray', 'orange', 'blue', 'red']
+        točke = []
+
+        for i, tijelo in enumerate(self.tijela):
+            točka, = ax.plot([], [], 'o', color=boje[i], label=tijelo.ime, markersize=4)
+            točke.append(točka)
+
+        def animiraj(frame):
+            for j, tijelo in enumerate(self.tijela):
+                if frame < len(tijelo.r):
+                    točke[j].set_data([tijelo.r[frame][0]], [tijelo.r[frame][1]])
+            return točke
+
+        ax.legend()
+        for i,tijelo in enumerate(self.tijela):
+            r,v,m,ime=tijelo.pod_za_gib()
+            linija, =ax.plot([r[0] for r in tijelo.r],[r[1] for r in tijelo.r],ls='--',color=boje[i],lw=1)
+            linija.set_dashes([8,12])
+        animacija = FuncAnimation(fig, animiraj, frames=range(0,len(self.tijela[0].r),3), interval=0.01, blit=True, repeat=False)
+        plt.xlabel("x [m]")
+        plt.ylabel("y [m]")
+        plt.show()
+
+
+Sunce=Planet(1.989e30,[0,0],[0,0],0,"Sunce")
+Merkur=Planet(3.285e23,[57.91e9,0],[0,47.9e3],120,"Merkur")
+Venera=Planet(4.867e24,[108.2e9,0],[0,35.2e3],265,"Venera")
+Zemlja=Planet(6e24,[149.6e9,0],[0,29.8e3],365.24,"Zemlja")
+Mars=Planet(6.4191e23,[228e9,0],[0,24.077e3],413,"Mars")
+Sun_sustav=Universe()
+Sun_sustav.dodaj_tijela(Sunce,Merkur,Venera,Zemlja,Mars) 
+
+Sun_sustav.simulacija(14400,1)
